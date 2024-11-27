fcfff4f8d76a1396d522c19bf611fb2bd3b5b088
ndubs18 <nick.winkler1@gmail.com>
2024-11-27 10:30:38 -0800

    checking for null playlist element

--- src/components/SpotifyPlaylists/SpotifyPlaylists.tsx
+++ src/components/SpotifyPlaylists/SpotifyPlaylists.tsx
@@ -1,4 +1,4 @@
-import {Profile} from '../../types/types';
+import { Profile } from '../../types/types';
 import { useState, useEffect } from 'react';
 import { useSource } from "../../App";
 import { SpotifyPlaylistCard } from '../SpotifyPlaylistCard/SpotifyPlaylistCard'
@@ -8,16 +8,16 @@ import noArtImg from '../../assets/images/noArtwork.png'
 import { getCurrentUserProfile, getCurrentUsersPlaylits, parseAccessToken } from '../../spotify/spotify';
 
 interface IPlaylists {
-    items : IPlaylist[]
+    items: IPlaylist[]
 
 }
 interface IPlaylist {
-    name : string,
-    snapshot_id : string,
-    images : {url : string}[],
-    id : string,
+    name: string,
+    snapshot_id: string,
+    images: { url: string }[],
+    id: string,
     owner: {
-        display_name : string,
+        display_name: string,
         id: string
     }
 }
@@ -25,7 +25,7 @@ interface IPlaylist {
 let SpotifyPlaylists = () => {
 
     //grab the context value
-    const {sourcePlatform, setSourcePlaylist} = useSource();
+    const { sourcePlatform, setSourcePlaylist } = useSource();
 
     let [profile, setProfile] = useState<Profile>();
     let [playlists, setPlaylists] = useState<IPlaylists | null>(null);
@@ -44,43 +44,47 @@ let SpotifyPlaylists = () => {
     return (
         <div>
             <h1>Spotify Playlists</h1>
-            {sourcePlatform === 'Spotify' ? 
-            <>
-                <h2>Choose a playlist to transfer</h2>
-                <ul className={styles.playlists}>
-                    {loading ? <h3>Loading...</h3> :
-                    playlists?.items.map(playlist => <li key={playlist.id}>
-                        <SpotifyPlaylistCard playlistId={playlist.id}
-                         name={playlist.name ? playlist.name : 'null'} 
-                         owner={playlist.owner.display_name} 
-                         imgUrl={playlist.images ? playlist.images[0].url : noArtImg } 
-                        /> 
-                        </li>
-                        )
-                    }
-                </ul> 
-            </> :
-           <>
-           <h2>Where would you like to transfer this playlist?</h2>
-           <ul className={styles.playlists}>
-               {loading ? <h3>Loading...</h3> :
-                    playlists?.items.map(playlist => {
-                        if(playlist.owner.id === profile?.id) { 
-                        return <li key={playlist.id}>
-                        <SpotifyPlaylistCard playlistId={playlist.id} 
-                        name={playlist.name ? playlist.name : 'null'} 
-                        owner={playlist.owner.display_name} 
-                        imgUrl={playlist.images ? playlist.images[0].url : noArtImg } 
-                        /> 
-                        </li>
+            {sourcePlatform === 'Spotify' ?
+                <>
+                    <h2>Choose a playlist to transfer</h2>
+                    <ul className={styles.playlists}>
+                        {loading ? <h3>Loading...</h3> :
+                            playlists?.items.map(playlist => {
+                                if (playlist !== null) {
+                                    return <li key={playlist.id}>
+                                        <SpotifyPlaylistCard playlistId={playlist.id}
+                                            name={playlist.name ? playlist.name : 'null'}
+                                            owner={playlist.owner.display_name}
+                                            imgUrl={playlist.images ? playlist.images[0].url : noArtImg}
+                                        />
+                                    </li>
+                                }
+                            }
+                            )
                         }
+                    </ul>
+                </> :
+                <>
+                    <h2>Where would you like to transfer this playlist?</h2>
+                    <ul className={styles.playlists}>
+                        {loading ? <h3>Loading...</h3> :
+                            playlists?.items.map(playlist => {
+                                if (playlist !== null && playlist.owner.id === profile?.id) {
+                                    return <li key={playlist.id}>
+                                        <SpotifyPlaylistCard playlistId={playlist.id}
+                                            name={playlist.name ? playlist.name : 'null'}
+                                            owner={playlist.owner.display_name}
+                                            imgUrl={playlist.images ? playlist.images[0].url : noArtImg}
+                                        />
+                                    </li>
+                                }
 
-                        else return null;
-                    })
-                }
-           </ul> 
-           </> 
-            }   
+                                else return null;
+                            })
+                        }
+                    </ul>
+                </>
+            }
         </div>
     )
 }
