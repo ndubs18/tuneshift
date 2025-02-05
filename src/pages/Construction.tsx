486eeba4977ff2569d256051cd5b957933cf44f0
ndubs18 <nick.winkler1@gmail.com>
2025-02-04 16:18:04 -0800

    styled construction page

--- src/pages/Construction.tsx
+++ src/pages/Construction.tsx
@@ -1,25 +1,9 @@
 import construction from '../assets/images/construct.jpg'
-
-
-let styles = {
-
-  img: {
-    height: '300px',
-    width: '500px',
-    margin: '0 auto',
-    borderRadius: '12px'
-
-  },
-  div: {
-    width: '500px',
-    height: '300px',
-    margin: '0 auto'
-  }
-}
+import styles from './Construction.module.css'
 
 let Construction = () => {
   return (
-    <div style={styles.div}><img style={styles.img} src={construction} /></div>
+    <div className={styles.constructionContainer}><img className={styles.constructionImg} src={construction} /></div>
   )
 }
 
